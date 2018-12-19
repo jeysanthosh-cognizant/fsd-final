473fa1bd93fb5fd67cab9f8d9e7222e65c18df8c
Santhosh, Jey S <vn68358@heb.com>
2018-12-19 16:54:54 -0600

    added spring boot servlet initialization

--- src/main/java/com/fsd/finalproj/projectmanagerapi/ProjectManagerApiApplication.java
+++ src/main/java/com/fsd/finalproj/projectmanagerapi/ProjectManagerApiApplication.java
@@ -3,11 +3,18 @@ package com.fsd.finalproj.projectmanagerapi;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @SpringBootApplication
-public class ProjectManagerApiApplication {
+public class ProjectManagerApiApplication extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application){
+		return application.sources(ProjectManagerApiApplication.class);
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(ProjectManagerApiApplication.class, args);
